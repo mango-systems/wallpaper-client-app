64f38171b978ea66125c0736acc329d43312e476
MSM74588 <msm74588@gmail.com>
2023-03-17 03:26:12 +0530

    experimenting with sidecar binary compilation

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+uncompiled_binaries_folder="./src-tauri/uncompiled-binaries"
+compiled_binary_folder_path="./src-tauri/binaries"
+
+pyinstaller_workdir="./src-tauri/uncompiled-binaries/build_files/pyinstaller_work"
+# pyinstaller_distdir="./src-tauri/uncompiled-binaries/build_files/pyinstaller_dist"
+pyinstaller_distdir="./src-tauri/binaries/"
+# echo "$(pwd)"
+
+# echo $uncompiled_binaries_folder
+
+function build_binaries (){
+    for binary in "$uncompiled_binaries_folder"/*
+do
+    # pyinstaller "$binary"
+    pyinstaller --onefile --distpath $pyinstaller_distdir --workpath $pyinstaller_workdir $binary
+    # echo $binary
+    echo $binary
+done
+}
+
+
+if [ -d "$compiled_binary_folder_path" ]; then
+  # If the folder exists, run your command
+  build_binaries
+else
+  # If the folder doesn't exist, create it and run your command
+  mkdir "$compiled_binary_folder_path"
+  build_binaries
+fi
+ 
+# npm run tauri build
+
+# add compiled binaries to .gitignore as they are platform specific
+
---
+++ src-tauri/uncompiled-binaries/setWinWallpaper.py
@@ -0,0 +1,19 @@
+import sys
+import ctypes
+
+SPI_SETDESKWALLPAPER = 20
+
+def set_wallpaper(path):
+    ctypes.windll.user32.SystemParametersInfoW(
+        SPI_SETDESKWALLPAPER, 0, path, 0)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python set_wallpaper.py <wallpaper_path>")
+        sys.exit(1)
+
+    wallpaper_path = sys.argv[1]
+    set_wallpaper(wallpaper_path)
+
+# python set_wallpaper.py path\to\image.jpg
+# compile into binary using pyinstaller
